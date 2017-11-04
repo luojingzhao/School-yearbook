9da52e68bfd65583a968630f575ec0842dac7bb2
Unknown <1009224322@qq.com>
2017-11-04 10:33:54 +0800

    feat(详情页): 完成详情页界面
    - 加入FloatgActionBar
    - 加入基本的编辑框

---
+++ School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/NewPersonActivity.java
@@ -0,0 +1,13 @@
+package com.example.xiong123.newperson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_person);
+    }
+}
