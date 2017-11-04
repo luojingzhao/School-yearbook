1a921c06c95a39b4b4b3c35153ba4f7b05ffd6aa
Unknown <1009224322@qq.com>
2017-11-04 20:49:26 +0800

    feat(邮件发送页): 增加了邮件发送页界面
    - 增加了邮件发送页界面

---
+++ School-yearbook/app/src/main/java/org/swsd/school_yearbook/presenter/IPresenterSendEmail.java
@@ -0,0 +1,11 @@
+package org.swsd.school_yearbook.presenter;
+
+/**
+ * author     :  熊立强
+ * time       :  2017/11/02
+ * description:  提供发送按钮的Presenter
+ * version:   :  1.0
+ */
+public interface IPresenterSendEmail {
+    public void sendEmail();
+}
---
+++ School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/SendEmailActivity.java
@@ -0,0 +1,15 @@
+package org.swsd.school_yearbook.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.swsd.school_yearbook.R;
+
+public class SendEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_email);
+    }
+}
