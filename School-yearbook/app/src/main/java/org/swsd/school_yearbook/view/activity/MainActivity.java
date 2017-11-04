2eb4f49ac45e0df2006a60433e0df7b0d0cf542f
Unknown <384282882@qq.com>
2017-11-04 21:38:58 +0800

    feat(主页): 设置跳转逻辑
    - 设置Menu跳转事件

--- School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/MainActivity.java
+++ School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/MainActivity.java
@@ -1,39 +1,83 @@
 package org.swsd.school_yearbook.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.swsd.school_yearbook.R;
-import org.swsd.school_yearbook.base.BaseActivity;
+import org.swsd.school_yearbook.presenter.adapter.NoteAdapter;
 
-public class MainActivity extends BaseActivity {
+/**
+ * author     :  骆景钊
+ * time       :  2017/11/04
+ * description:  RecyclerView在界面上的实现，title加号的点击实现
+ * version:   :  1.0
+ */
+
+public class MainActivity extends AppCompatActivity{
+
+    private ImageView addImaeView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    protected void initData() {
-
-        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        super.steepStatusBar();
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_main);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+        addImaeView = (ImageView) findViewById(R.id.iv_add_icon);
+        addImaeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(addImaeView);
+            }
+        });
 
     }
 
-    @Override
-    public int bindLayout(){
-        return 1;
+    private void showPopupMenu(ImageView addImaeView) {
+
+        // View当前PopupMenu显示的相对View的位置
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, addImaeView);
+
+        // menu布局
+        getMenuInflater().inflate(R.menu.menu, popupMenu.getMenu());
+        //给菜单绑定监听器
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.add_item:
+                        goAddNewPerson();
+                        break;
+                    case R.id.excel_item:
+                        Toast.makeText(MainActivity.this, "导出excel成功，请在文件管理器中查看", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.photo_item:
+                        Toast.makeText(MainActivity.this, "导出jpg成功，请在文件管理器中查看", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
     }
-   @Override
-    public  void initView(){
 
+    // 进入到新建同学录界面
+    private void goAddNewPerson(){
+        Intent intent = new Intent(MainActivity.this,NewPersonActivity.class);
+        startActivity(intent);
     }
-
-   
-
 }
