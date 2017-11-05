2783d79b68da299dde94e7ba51a4cef3bd4f2a3a
xiong123 <1009224322@qq.com>
2017-11-05 10:32:54 +0800

    fix(主页): 修复闪退
    - 修改了 Manifest 的错误

--- School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/MainActivity.java
+++ School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/MainActivity.java
@@ -1,16 +1,20 @@
 package org.swsd.school_yearbook.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.swsd.school_yearbook.R;
 import org.swsd.school_yearbook.presenter.adapter.NoteAdapter;
+
 
 /**
  * author     :  骆景钊
@@ -27,8 +31,8 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_main);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_main);
         recyclerView.setLayoutManager(layoutManager);
         NoteAdapter adapter = new NoteAdapter();
         recyclerView.setAdapter(adapter);
@@ -39,18 +43,41 @@ public class MainActivity extends AppCompatActivity{
                 showPopupMenu(addImaeView);
             }
         });
-
     }
 
     private void showPopupMenu(ImageView addImaeView) {
 
         // View当前PopupMenu显示的相对View的位置
-        PopupMenu popupMenu = new PopupMenu(this, addImaeView);
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, addImaeView);
 
         // menu布局
-        popupMenu.getMenuInflater().inflate(R.menu.add_item, popupMenu.getMenu());
-
-
+        getMenuInflater().inflate(R.menu.menu, popupMenu.getMenu());
+        //给菜单绑定监听器
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.add_item:
+                        goAddNewPerson();
+                        break;
+                    case R.id.excel_item:
+                        Toast.makeText(MainActivity.this, "导出excel成功，请在文件管理器中查看", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.photo_item:
+                        Toast.makeText(MainActivity.this, "导出jpg成功，请在文件管理器中查看", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
         popupMenu.show();
+    }
+
+    // 进入到新建同学录界面
+    private void goAddNewPerson(){
+        Intent intent = new Intent(MainActivity.this,NewPersonActivity.class);
+        startActivity(intent);
     }
 }
---
+++ School-yearbook/app/src/main/java/org/swsd/school_yearbook/view/activity/NoteAddActitvity.java
@@ -0,0 +1,23 @@
+package org.swsd.school_yearbook.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class NoteAddActitvity extends AppCompatActivity {
+    private EditText et_contact_name = null;
+    private EditText et_contact_address = null;
+    private EditText et_contact_phone = null;
+    private EditText et_contact_wechat = null;
+    private EditText et_contact_email = null;
+    private EditText et_contact_qq = null;
+    private EditText et_contact_assgin = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
